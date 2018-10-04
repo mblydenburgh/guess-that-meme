e766b0230b914891d6b79e863c589c7e86799346
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-04 17:12:18 -0400

    reformatted with functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 // Define variables to use
+//
 const names = ["doge", "grumpy cat", "dat boi", "scumbag steve", "distracted boyfriend", "aliens", "philosoraptor", "success kid", "dramatic prarie dog", "squinting fry", "thanks obama", "sarcastic willy wonka", "nyan cat", "crying michael jordan", "harambe", "u mad bro", "rick rolling", "all your base are belong to us", "trollface"];
 let guessedLetters = [];
 let lives = 10;
@@ -7,90 +8,114 @@ const guessedLetterSpan = document.getElementById("guessed");
 const blankWordP = document.getElementById("blankWordDisplay");
 const lifeDisplaySpan = document.getElementById("lifeDisplay");
 const letterRegex = /[a-z]/;
-const keyFilter = ["shift", "alt", "control", "arrowdown", "arrowup", "arrowleft", "arrowright", "meta", "space","escape"];
+const keyFilter = ["shift", "alt", "control", "arrowdown", "arrowup", "arrowleft", "arrowright", "meta", "space", "escape", "enter", "tab"];
 
-//choose random number to act as an index for the names array
-let wordIndex = Math.floor(Math.random() * names.length);
-console.log(names[wordIndex]);
-let secretWord = names[wordIndex];
-let blankWord = []
+// The game's functions are defined below
+//
+let newGame = function gameStart() {
 
-//loop through the randomly chosen word to make a blank to display on game start
-for (let i = 0; i < secretWord.length; i++) {
-    if(secretWord.charCodeAt(i) === 32){
-        blankWord.push(" ");
-    }else{
-        blankWord.push("_")
+    let wordIndex = Math.floor(Math.random() * names.length);
+    //console.log(names[wordIndex]);
+    let secretWord = names[wordIndex];
+    //console.log(`secret word: ${secretWord}`)
+    let blankWord = makeBlankWord(secretWord);
+    //console.log(blankWord);
+    let words = {
+        secretWord: names[wordIndex],
+        blankWord: blankWord
     }
-    
+
+
+    blankWordP.textContent = words.blankWord.join('');
+    lifeDisplaySpan.textContent = lives;
+    console.log(words)
+    return words;
 }
 
-//console.log(blankWord.join(''));
-blankWordP.textContent = blankWord.join('');
-lifeDisplaySpan.textContent = lives;
-
-//listen to key presses, execute code when a letter is hit
-document.onkeyup = function (event) {
-    let keyPressed = event.key.toLowerCase();
-
-    /* Used charCodeAt to filter key presses originally-
-     if(keyPressed.charCodeAt(0) >= 87 && keyPressed.charCodeAt(0) <= 122){
-         console.log(`letter hit`);
-     } else{
-         console.log(`not a letter hit`);
-     }*/
-
-
-    /*Notes about things to implement:
-        replace all instances of correct guessed letter, not just first
-        when blankWord === secretWord, win
-        loop while lives > 0
-    */
-
-    // the outter if state validates key presses, only executing game code for letters hit, ignoring all numbers, symbols, and other keys
-    if (letterRegex.test(keyPressed) && !keyFilter.includes(keyPressed)) {
-        //console.log(`letter hit`);
-        //If letter has not been guessed yet, add to array and check names. If key has been guessed already, nothing will happen
-        if (guessedLetters.includes(keyPressed)) {
-            console.log(`Already guessed ${keyPressed}`);
+let makeBlankWord = function makeBlankWord(str) {
+    var blankWord = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) === 32) {
+            blankWord.push(" ");
         } else {
-            guessedLetters.push(keyPressed);
-            console.log(keyPressed);
-            if (secretWord.includes(keyPressed)) {
-                // pressed key in word - display the letter and continue
-                // console.log(`${keyPressed} is in the secret word`);
-                // let index = secretWord.indexOf(keyPressed);
-                // console.log(`${keyPressed} is in the ${index} index`);
-                // blankWord[index] = keyPressed;
-                // blankWordP.textContent = blankWord.join('');
-
-                // since a word can have more than one instance of a letter, 
-                let indexArr = [];
-                for(let i = 0; i < secretWord.length; i++){
-                    if(secretWord[i] === keyPressed){
-                        indexArr.push(i);
-                    }
-                }
-                console.log(`indexes for ${keyPressed} in ${secretWord} are ${indexArr}`);
-
-                for(let i = 0; i < indexArr.length; i++){ //Loop through indexArr to replace all instances of correct letter
-                    blankWord[indexArr[i]] = keyPressed;
-                }
-                blankWordP.textContent = blankWord.join('');
-
-            } else {
-                // pressed key is not in word - take away life
-                console.log(`${keyPressed} is not in the secret word`);
-                lives = lives - 1;
-            }
+            blankWord.push("_")
         }
-    } else {
-        //console.log(`non letter hit`);
+
     }
+    return blankWord;
+}
 
-
-
-
+function restartGame() {
+    lives = 10;
+    guessedLetters = [];
     lifeDisplaySpan.textContent = lives;
-    guessedLetterSpan.textContent = guessedLetters; //update screen with guessed letters for user to see
-} // end keyup event
+    guessedLetterSpan.textContent = guessedLetters;
+    words = newGame();
+    return words;
+}
+
+window.onload = function () {
+    let words = newGame(); //
+    console.log(`secret word: ${words.secretWord}`);
+
+
+
+    //listen to key presses, execute code when a letter is hit
+    document.onkeyup = function (event) {
+
+        let keyPressed = event.key.toLowerCase(); // in case caps-lock is on
+
+        if (lives > 0) { // check lives on each keypress
+            if (words.secretWord !== words.blankWord.join('')) { // check if the blankword equals the chosen word, if they do, player wins
+                // the outter if state validates key presses, only executing game code for letters hit, ignoring all numbers, symbols, and other keys
+                if (letterRegex.test(keyPressed) && !keyFilter.includes(keyPressed)) {
+                    //console.log(`letter hit`);
+                    //If letter has not been guessed yet, add to array and check names. If key has been guessed already, nothing will happen
+                    if (guessedLetters.includes(keyPressed)) {
+                        console.log(`Already guessed ${keyPressed}`);
+                    } else {
+                        guessedLetters.push(keyPressed);
+                        console.log(keyPressed);
+                        if (words.secretWord.includes(keyPressed)) {
+                            // pressed key in word - display the letter and continue
+                            // since a word can have more than one instance of a letter, 
+                            let indexArr = [];
+                            for (let i = 0; i < words.secretWord.length; i++) {
+                                if (words.secretWord[i] === keyPressed) {
+                                    indexArr.push(i);
+                                }
+                            }
+                            console.log(`indexes for ${keyPressed} in ${words.secretWord} are ${indexArr}`);
+
+                            for (let i = 0; i < indexArr.length; i++) { //Loop through indexArr to replace all instances of correct letter
+                                words.blankWord[indexArr[i]] = keyPressed;
+                            }
+                            let solvedWord = words.blankWord.join('');
+                            blankWordP.textContent = solvedWord;
+
+                        } else {
+                            // pressed key is not in word - take away life
+                            console.log(`${keyPressed} is not in the secret word`);
+                            lives = lives - 1;
+                        }
+                    }
+                } else {
+                    //console.log(`non letter hit`);
+                }
+
+                //update lives and any guessed letters
+                lifeDisplaySpan.textContent = lives;
+                guessedLetterSpan.textContent = guessedLetters;
+            } else { // the blank word equals the chosen word - player wins
+                console.log(words);
+                alert(`Winner winner chicken dinner!`);
+                words = restartGame();
+            }
+
+        } else { // no lives left
+            alert(`game over man!`)
+            words = restartGame();
+        }
+    }// end keyup event
+
+}// end parent window.onload funcion
